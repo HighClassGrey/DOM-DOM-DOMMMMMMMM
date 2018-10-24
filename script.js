ad687747b6640477354d47a033a8cc896a0e2f2a
HighClassGrey <tamekaahannah@gmail.com>
2018-10-24 12:09:35 -0500

    up

--- script.js
+++ script.js
@@ -41,11 +41,30 @@ function divAdd() {
         squareDiv.innerText = "  ";
     });
 
-};
+    squareDiv.addEventListener('dblclick', function () {
+        checkEven()
 
-//random colors function
-function randomColor() {
-    let colors = ['white','red' ];
-    let randomNumber = Math.floor((Math.random() * colors.length) + 0);
-    return colors[randomNumber];
+    });
+
+
+    // change the value of number to test your Code
+
+    //If number equals 1, then odd should be printed to the console.
+    //If number equals 2, then Even should be printed to the console.
+
+
+    function checkEven() {
+        if (this.id >2 ) {
+            console.log('more');
+        } else {
+            console.log('less');
+        }
+    };
+
+    //random colors function
+    function randomColor() {
+        let colors = ['white', 'red'];
+        let randomNumber = Math.floor((Math.random() * colors.length) + 0);
+        return colors[randomNumber];
+    };
 };
